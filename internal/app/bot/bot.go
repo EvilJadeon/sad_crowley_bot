82313e579bb2f19fe5d63f5fa11c4208c158d4f3
EvilJadeon <thejadeonwot@gmail.com>
2025-04-02 19:21:34 +0300

    Add some logging

--- internal/app/bot/bot.go
+++ internal/app/bot/bot.go
@@ -1,15 +1,15 @@
 package bot
 
 import (
-	"log"
-
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/sirupsen/logrus"
 )
 
 // Bot ...
 type Bot struct {
 	config *Config
 	api    *tgbotapi.BotAPI
+	logger *logrus.Logger
 }
 
 // New ...
@@ -17,20 +17,38 @@ func New(config *Config) *Bot {
 	return &Bot{
 		config: config,
 		api:    nil,
+		logger: logrus.New(),
 	}
+}
+
+func (b *Bot) SetLogger() error {
+	level, err := logrus.ParseLevel(b.config.LogLevel)
+
+	if err != nil {
+		return err
+	}
+
+	b.logger.SetLevel(level)
+
+	return nil
 }
 
 // Start ...
 func (b *Bot) Start() error {
+	if err := b.SetLogger(); err != nil {
+		return err
+	}
+
 	bot, err := tgbotapi.NewBotAPI(b.config.TelegramBotToken)
+
 	if err != nil {
-		log.Fatal(err)
+		b.logger.Errorf("Failed to create bot: %v", err)
 	}
 
 	b.api = bot
 	bot.Debug = true
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	b.logger.Infof("Authorized on account %s", bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -47,11 +65,11 @@ func (b *Bot) Start() error {
 
 func (b *Bot) handleMessage(update tgbotapi.Update) {
 	if update.Message != nil {
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		b.logger.Infof("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "I received your message: "+update.Message.Text)
 		if _, err := b.api.Send(msg); err != nil {
-			log.Printf("Error sending message: %v", err)
+			b.logger.Errorf("Error sending message: %v", err)
 		}
 	}
 }
@@ -72,7 +90,7 @@ func (b *Bot) handleCommand(update tgbotapi.Update) {
 func (b *Bot) handleStartCommand(update tgbotapi.Update) {
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Welcome! I'm your bot. Use /help to see available commands.")
 	if _, err := b.api.Send(msg); err != nil {
-		log.Printf("Error sending start message: %v", err)
+		b.logger.Errorf("Error sending start message: %v", err)
 	}
 }
 
@@ -83,13 +101,13 @@ func (b *Bot) handleHelpCommand(update tgbotapi.Update) {
 
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, helpText)
 	if _, err := b.api.Send(msg); err != nil {
-		log.Printf("Error sending help message: %v", err)
+		b.logger.Errorf("Error sending help message: %v", err)
 	}
 }
 
 func (b *Bot) handleUnknownCommand(update tgbotapi.Update) {
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Unknown command. Use /help to see available commands.")
 	if _, err := b.api.Send(msg); err != nil {
-		log.Printf("Error sending unknown command message: %v", err)
+		b.logger.Errorf("Error sending unknown command message: %v", err)
 	}
 }
--- internal/app/bot/config.go
+++ internal/app/bot/config.go
@@ -5,11 +5,13 @@ import "os"
 // Config ...
 type Config struct {
 	TelegramBotToken string `env:"TELEGRAM_BOT_API_TOKEN"`
+	LogLevel         string `env:"LOG_LEVEL"`
 }
 
 // NewConfig ...
 func NewConfig() *Config {
 	return &Config{
 		TelegramBotToken: os.Getenv("TELEGRAM_BOT_API_TOKEN"),
+		LogLevel:         os.Getenv("LOG_LEVEL"),
 	}
 }
